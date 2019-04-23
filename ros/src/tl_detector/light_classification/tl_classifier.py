e7cb89c967658b025b9fd7ba6f2725fcfbf8298e
Bharath-Kumar-G <48758396+Bharath-Kumar-G@users.noreply.github.com>
2019-04-23 23:06:04 +0530

    update tl_classifier
    Implement basic tl classifier using openCV

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,4 +1,6 @@
 from styx_msgs.msg import TrafficLight
+import numpy as np
+import cv2
 
 class TLClassifier(object):
     def __init__(self):
@@ -15,5 +17,41 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
+        # convert the received image to HSV colorspace
+        hsv_image = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+
+        # The hue range of any color can be found as described in openCV documentation -
+        # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_colorspaces/py_colorspaces.html # noqa
+
+        # Hue range of Red is between 0 -10 & 160 - 180
+        lower_red_1 = np.array([0, 100, 100],np.uint8)
+        upper_red_1 = np.array([10, 255, 255],np.uint8)        
+
+        lower_red_2 = np.array([160, 100, 100],np.uint8)
+        upper_red_2 = np.array([180, 255, 255],np.uint8)
+
+        mask_red_1 = cv2.inRange(hsv_image, lower_red_1, upper_red_1) 
+        mask_red_2 = cv2.inRange(hsv_image, lower_red_2, upper_red_2) 
+
+        if cv2.countNonZero(mask_red_1) + cv2.countNonZero(mask_red_2) > 50:
+            return TrafficLight.RED
+        
+        # Hue range of yellow is between 20 - 30
+        lower_yellow = np.array([20, 100, 100],np.uint8)
+        upper_yellow = np.array([30, 255, 255],np.uint8)
+
+        mask_yellow = cv2.inRange(hsv_image, lower_yellow, upper_yellow)
+
+        if cv2.countNonZero(mask_yellow) > 50:
+            return TrafficLight.YELLOW
+        
+        # Hue range of green is between 50 - 70
+        lower_green = np.array([50, 100, 100],np.uint8)
+        upper_green = np.array([70, 255, 255],np.uint8)
+
+        mask_green = cv2.inRange(hsv_image, lower_green, upper_green)
+
+        if cv2.countNonZero(mask_green) > 50:
+            return TrafficLight.GREEN
+
         return TrafficLight.UNKNOWN
